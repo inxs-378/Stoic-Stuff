d6878cf8474954e5b2d7206f1fa48fa554ab5438
inxs-378 <59421368+inxs-378@users.noreply.github.com>
2020-03-09 22:47:27 -0400

    Add files via upload

---
+++ problemas estoico.py
@@ -0,0 +1,47 @@
+print ('Bienvenido al resolvedor de problemas estoicos')
+print ("Respuestas validas afirmativas: y,n,in part")
+print ("el problema esta bajo tu control?")
+x = input("R:")
+if x == "y":
+    print ("¿concierne a tu virtud?")
+    x2 = input("R:")
+    if x2 == 'si':
+        print ("Utilícelo para la práctica: sabiduria, justicia,templanza y coraje")
+elif x == "in part":
+    print ("qué concierne a nuestro horizonte de decisión?")
+    print ("El intento o el resultado?")
+    x3 = input ("R:")
+else:
+    print ("no hay nada por hacer")
+
+
+
+        #print: ("¿es indiferente para mi?")
+#else:
+    #if x == 'en parte' and 'mas o menos' and 'no se':
+       # 
+       # 
+        #if x3 == "El intento":
+           # print ("¿concierne a la virtud?")
+           # x2 = input("R:")
+            #if x2 == "si":
+               # print ("Utilícelo para la práctica: sabiduria, justicia,templanza y coraje")
+           # else:
+              #  print: ("es indiferente para mi?")
+       # else:
+           # print ("no hay nada por hacer")
+   # else:
+       # 
+#El diagrama comienza por la sencilla pregunta de si el dilema existencial al que nos enfrentamos 
+#está o no bajo nuestro control. Si no, no hay nada qué hacer; si en parte, hay que saber distinguir 
+#qué concierne a nuestro horizonte de decisión, si el intento o el resultado; si nos involucra por completo,
+#la siguiente pregunta es si la decisión involucra a la virtud; si no, es en cierta forma indiferente para
+#nuestra existencia y, si acaso el único momento de dilema es si dicha decisión entra en conflicto con la sabiduría,
+#la justicia, la templanza o la valentía; si es una decisión que concierne de lleno a la virtud, un estoico
+#te recomendaría tomarla para ejercer estos mismos valores.
+
+#Al final, esto último es lo más sustancioso. Ante una decisión pregúntate si tomarla requerirá de tu sabiduría,
+#tu sentido de la justicia, tu templanza o tu valentía, o si una vez tomada serás más sabio, más justo, más ecuánime
+#o más valiente. Si la respuesta es afirmativa, ¡toma la decisión! Probablemente después descubrirás que hacerlo valió
+#la pena, que tus temores no eran tan terribles como lo supusiste y, lo más importante, que seguramente tu carácter es 
+#más fuerte de lo que creías.
